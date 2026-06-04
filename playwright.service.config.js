bb567fe3cc7d5b358a577eb2e5abfa4721bfbd36
subasish <subasishsahu70@gmail.com>
2026-06-04 16:47:03 +0530

    azure integration

---
+++ playwright.service.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('@playwright/test');
+const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
+const { DefaultAzureCredential } = require('@azure/identity');
+const config = require('./playwright.config');
+
+module.exports = defineConfig(
+  config,
+  createAzurePlaywrightConfig(config, {
+    exposeNetwork: '<loopback>',
+    connectTimeout: 3 * 60 * 1000,
+    os: ServiceOS.LINUX,
+    credential: new DefaultAzureCredential(),
+  }),
+  {
+    reporter: [
+      ["html", { open: "never" }],
+      ["@azure/playwright/reporter"],
+    ],
+  }
+);
